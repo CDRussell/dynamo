ca492f43f17d5d7829186e9ce914a1a9f83e03bf
Craig Russell <CDRussell@users.noreply.github.com>
2020-06-17 17:21:51 +0100

    Generating passwords from iOS too

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
 import SharedCode
 
+typealias PasswordSuccess = PasswordGenerator.PasswordResultPasswordSuccess
+typealias PasswordFailure = PasswordGenerator.PasswordResultPasswordFailure
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -10,6 +13,21 @@ class ViewController: UIViewController {
         label.textAlignment = .center
         label.font = label.font.withSize(25)
         label.text = CommonKt.createApplicationScreenMessage()
+        
+        let config = PasswordGenerator.PasswordConfiguration(requiredLength:12,
+                                                             numericalType: PasswordGenerator.NumericalType(included: true, minimumCharacters: 0),
+                                                             upperCaseLetterType: PasswordGenerator.UpperCaseLetterType(included: true, minimumCharacters: 0),
+                                                             lowerCaseLetterType: PasswordGenerator.LowerCaseLetterType(included: true, minimumCharacters: 0),
+                                                             specialCharacterLetterType: PasswordGenerator.SpecialCharacterLetterType(included: true, minimumCharacters: 0))
+        
+        let result = PasswordGenerator().generatePassword(passwordConfiguration: config)
+        
+        if let result = result as? PasswordSuccess {
+            label.text = result.password
+        } else if let result = result as? PasswordFailure {
+            label.text = result.exception.message
+        }
+        
         view.addSubview(label)
     }
 }

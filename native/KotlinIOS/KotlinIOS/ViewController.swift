78636d4ba4de311c4942f1f628a00f39436249a8
Craig Russell <CDRussell@users.noreply.github.com>
2020-06-18 13:18:00 +0100

    Able to generate passwords on iOS on demand

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -5,29 +5,40 @@ typealias PasswordSuccess = PasswordGenerator.PasswordResultPasswordSuccess
 typealias PasswordFailure = PasswordGenerator.PasswordResultPasswordFailure
 
 class ViewController: UIViewController {
+        
+    @IBAction func generatePasswordButtonPressed(_ sender: Any) {
+        generatePassword()
+    }
+    
+    @IBOutlet weak var passwordField: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        label.font = label.font.withSize(25)
-        label.text = CommonKt.createApplicationScreenMessage()
+//        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+//        label.center = CGPoint(x: 160, y: 285)
+//        label.textAlignment = .center
+//        label.font = label.font.withSize(25)
+//        label.text = CommonKt.createApplicationScreenMessage()
+//        view.addSubview(label)
         
-        let config = PasswordGenerator.PasswordConfiguration(requiredLength:12,
-                                                             numericalType: PasswordGenerator.NumericalType(included: true, minimumCharacters: 0),
-                                                             upperCaseLetterType: PasswordGenerator.UpperCaseLetterType(included: true, minimumCharacters: 0),
-                                                             lowerCaseLetterType: PasswordGenerator.LowerCaseLetterType(included: true, minimumCharacters: 0),
-                                                             specialCharacterLetterType: PasswordGenerator.SpecialCharacterLetterType(included: true, minimumCharacters: 0))
-        
-        let result = PasswordGenerator().generatePassword(passwordConfiguration: config)
-        
-        if let result = result as? PasswordSuccess {
-            label.text = result.password
-        } else if let result = result as? PasswordFailure {
-            label.text = result.exception.message
-        }
-        
-        view.addSubview(label)
+       generatePassword()
+    }
+    
+    func generatePassword() {
+       let config = PasswordGenerator.PasswordConfiguration(requiredLength:12,
+                                                            numericalType: PasswordGenerator.NumericalType(included: true, minimumCharacters: 0),
+                                                            upperCaseLetterType: PasswordGenerator.UpperCaseLetterType(included: true, minimumCharacters: 0),
+                                                            lowerCaseLetterType: PasswordGenerator.LowerCaseLetterType(included: true, minimumCharacters: 0),
+                                                            specialCharacterLetterType: PasswordGenerator.SpecialCharacterLetterType(included: true, minimumCharacters: 0))
+       
+       let result = PasswordGenerator().generatePassword(passwordConfiguration: config)
+       
+       if let result = result as? PasswordSuccess {
+           passwordField.text = result.password
+       } else if let result = result as? PasswordFailure {
+           passwordField.text = result.exception.message
+       }
     }
 }
